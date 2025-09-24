db2f7a73025497ea36324bbea756c7bf3bd31194
cdona005 <cdona005@odu.edu>
2025-09-23 20:15:53 -0600

    Connected database to Django Project

--- EduSense/EduSense/settings.py
+++ EduSense/EduSense/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'EduSense.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '__DATABASE__.db',
     }
 }
 
