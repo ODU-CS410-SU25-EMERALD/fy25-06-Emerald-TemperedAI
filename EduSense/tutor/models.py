d7200068f82eb5ca6f2f2dc73e0269b0940c2cbc
Dillon Sapp <87584441+klevvergirl@users.noreply.github.com>
2025-09-29 00:32:14 -0600

    Added Serializer for database

--- EduSense/tutor/models.py
+++ EduSense/tutor/models.py
@@ -1,18 +1,24 @@
+####################################################################################################   
 # __Database__.db Models
 # Author: Dillon Sapp
 #
 # This database was created utilizing DB Browser for SQLite and their respective Django models were 
 # initially auto-generated with Django's built-in 'inspectdb' command.
 # 'managed=false' has been commented out to allow Django to alter the models.
+####################################################################################################
 
 from django.db import models
 
 
 class Assignment(models.Model):
     assignment_id = models.AutoField(primary_key=True)
+    title = models.TextField(blank=True, null=True)
     due_date = models.TextField(blank=True, null=True)
     settings = models.TextField(blank=True, null=True)
     course = models.ForeignKey('Course', models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         #managed = False
@@ -24,6 +30,9 @@ class Conversation(models.Model):
     student = models.ForeignKey('Student', models.CASCADE)
     assignment = models.ForeignKey(Assignment, models.CASCADE)
 
+    def __str__(self):
+        return self.student
+
     class Meta:
         #managed = False
         db_table = 'Conversation'
@@ -31,7 +40,11 @@ class Conversation(models.Model):
 
 class Course(models.Model):
     course_id = models.AutoField(primary_key=True)
+    name = models.TextField(blank=True, null=True)
     settings = models.TextField(blank=True, null=True)
+
+    def __str_(self):
+        return self.name
 
     class Meta:
         #managed = False
@@ -45,6 +58,9 @@ class Instructor(models.Model):
     access_token = models.TextField(unique=True)
     courses = models.ManyToManyField(Course)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         #managed = False
         db_table = 'Instructor'
@@ -57,6 +73,9 @@ class Question(models.Model):
     assignment = models.ForeignKey(Assignment, models.CASCADE)
     conversation = models.ForeignKey(Conversation, models.DO_NOTHING, blank=True, null=True)
 
+    def __str__(self):
+        return self.question_text
+    
     class Meta:
         #managed = False
         db_table = 'Question'
@@ -69,6 +88,9 @@ class Response(models.Model):
     final_response = models.TextField()
     time = models.TextField()
     conversation = models.ForeignKey(Conversation, models.CASCADE)
+
+    def __str__(self):
+        return self.prompt
 
     class Meta:
         #managed = False
@@ -83,6 +105,9 @@ class Student(models.Model):
     courses = models.ManyToManyField(Course)
     access_token = models.TextField(unique=True)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         #managed = False
         db_table = 'Student'
@@ -91,6 +116,9 @@ class DjangoMigrations(models.Model):
     app = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     applied = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         #managed = False
---
+++ EduSense/tutor/serializers.py
@@ -0,0 +1,49 @@
+####################################################################################################   
+# __Database__.db Model Serializers
+# Author: Dillon Sapp
+#
+# This file contains the serializer classes used to convert each item in our database into JSON 
+####################################################################################################
+
+from rest_framework import serializers
+from .models import *
+
+class AssignmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Assignment
+        fields = '__all__'
+
+class ConversationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Conversation
+        fields = '__all__'
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class InstructorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Instructor
+        fields = '__all__'
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+class ResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Response
+        fields = '__all__'
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+class DjangoMigrationsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DjangoMigrations
+        fields = '__all__'
